0a527d15fb08992cf3f8f43679b3908596a814a0
Lucas Ransan <lucas@ransan.fr>
2023-10-18 15:47:13 +0200

    init

---
+++ src/main/kotlin/fr/paugesjanes/PaugesJanesApplication.kt
@@ -0,0 +1,11 @@
+package fr.paugesjanes
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PaugesJanesApplication
+
+fun main(args: Array<String>) {
+	runApplication<PaugesJanesApplication>(*args)
+}
---
+++ src/test/kotlin/fr/paugesjanes/PaugesJanesApplicationTests.kt
@@ -0,0 +1,13 @@
+package fr.paugesjanes
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PaugesJanesApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
