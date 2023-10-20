42263576114b86631bd9b64ec2ee5c1c77a0eb62
Lucas Ransan <lucas@ransan.fr>
2023-10-21 00:52:02 +0200

    use database for logging in

---
+++ src/main/kotlin/fr/paugesjanes/JpaUserDetailsService.kt
@@ -0,0 +1,23 @@
+package fr.paugesjanes
+
+import jakarta.transaction.Transactional
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.security.core.authority.SimpleGrantedAuthority
+import org.springframework.security.core.userdetails.User
+import org.springframework.security.core.userdetails.UserDetails
+import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.security.core.userdetails.UsernameNotFoundException
+import org.springframework.stereotype.Service
+
+@Service
+class JpaUserDetailsService(
+    @Autowired
+    private val userRepository: UserRepository,
+) : UserDetailsService {
+    @Transactional
+    override fun loadUserByUsername(username: String): UserDetails {
+        val user = userRepository.findByUsername(username) ?: throw UsernameNotFoundException(username)
+        val authorities = user.roles.map { SimpleGrantedAuthority("ROLE_" + it.name) }
+        return User(username, user.password, authorities)
+    }
+}
