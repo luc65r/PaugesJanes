1c7858c3ccdaaa8a097e98de9924fa8f855c56d6
Lucas Ransan <lucas@ransan.fr>
2023-10-21 15:41:33 +0200

    move templates and controllers

--- src/main/kotlin/fr/paugesjanes/web/MainController.kt
+++ src/main/kotlin/fr/paugesjanes/web/MainController.kt
@@ -1,15 +1,11 @@
-package fr.paugesjanes
+package fr.paugesjanes.web
 
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
-import org.springframework.ui.set
 import org.springframework.web.bind.annotation.GetMapping
 
 @Controller
-class HtmlController {
+class MainController {
     @GetMapping("/")
-    fun index(model: Model): String {
-        model["a"] = "couzi"
-        return "index"
-    }
+    fun index(model: Model): String = "main/index"
 }
