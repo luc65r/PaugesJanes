09385f6dd49acf2bd47ea8e34904bb4434f8296b
Lucas Ransan <lucas@ransan.fr>
2023-10-23 22:36:57 +0200

    create passwordEncoder bean

--- src/main/kotlin/fr/paugesjanes/config/SecurityConfig.kt
+++ src/main/kotlin/fr/paugesjanes/config/SecurityConfig.kt
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.invoke
+import org.springframework.security.crypto.factory.PasswordEncoderFactories
+import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.web.SecurityFilterChain
 
 @Configuration
@@ -28,4 +30,7 @@ class SecurityConfig {
         }
         return http.build()
     }
+
+    @Bean
+    fun passwordEncoder(): PasswordEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder()
 }
