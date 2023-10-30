4f855697e242d0e71a17e7f9a864f9fe11a4d748
jlb64 <jliogerbun+github@gmail.com>
2023-10-30 21:12:11 +0100

    Controller for the search bar

--- src/main/kotlin/fr/paugesjanes/web/MainController.kt
+++ src/main/kotlin/fr/paugesjanes/web/MainController.kt
@@ -1,11 +1,22 @@
 package fr.paugesjanes.web
 
+import fr.paugesjanes.User
+import fr.paugesjanes.UserRepository
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.core.annotation.MergedAnnotations.Search
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.ResponseBody
 
 @Controller
 class MainController {
+
+    @Autowired
+    private lateinit var userRepository: UserRepository
+
     @GetMapping("/")
     fun index(model: Model): String = "main/index"
 
@@ -16,6 +27,22 @@ class MainController {
     fun contact(): String = "main/contact"
 
     @GetMapping("/search")
-    fun search(): String = "main/search"
+    fun search(model: Model): String {
+        model.addAttribute("users", emptyList<User>()) // Initialize users as an empty list
+        return "main/search"
+    }
+
+    @PostMapping("/search")
+    @ResponseBody
+    fun searchProfile(@RequestParam("search") search: String): List<User> {
+        // Modify your repository method to search by username, portfolio, or project
+        val usersByUsername = userRepository.findByUsernameContaining(search)
+        val usersByPortfolio = userRepository.findByPortfolioContaining(search)
+
+        // Combine the results and remove duplicates if necessary
+        val searchResults = usersByUsername + usersByPortfolio
+        return searchResults.distinct()
+    }
+
 
 }
