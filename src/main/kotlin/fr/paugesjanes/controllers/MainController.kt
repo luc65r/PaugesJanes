3ac25df1ad2c78dc580c8f8a23e660c38cc84fde
Lucas Ransan <lucas@ransan.fr>
2023-11-05 23:58:54 +0100

    finish home page

--- src/main/kotlin/fr/paugesjanes/controllers/MainController.kt
+++ src/main/kotlin/fr/paugesjanes/controllers/MainController.kt
@@ -1,6 +1,10 @@
 package fr.paugesjanes.controllers
 
+import fr.paugesjanes.htmx
+import fr.paugesjanes.repositories.ProjectRepository
 import fr.paugesjanes.repositories.UserRepository
+import io.github.wimdeblauwe.htmx.spring.boot.mvc.HtmxResponse
+import io.github.wimdeblauwe.htmx.spring.boot.mvc.HxRequest
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
@@ -11,10 +15,20 @@ import java.security.Principal
 @Controller
 class MainController(
     @Autowired
-    var userRepository: UserRepository
+    var userRepository: UserRepository, private val projectRepository: ProjectRepository
 ) {
     @GetMapping("/")
-    fun index(model: Model): String = "main/index"
+    fun index(model: Model): String {
+        model["portfolio"] = projectRepository.findRandomPortfolio()
+        return "main/index"
+    }
+
+    @HxRequest
+    @GetMapping("/getRandomPortfolio")
+    fun getRandomPortfolio(model: Model): HtmxResponse {
+        model["portfolio"] = projectRepository.findRandomPortfolio()
+        return htmx { view("fragments/homeCard :: homeCard") }
+    }
 
     @GetMapping("/a_propos")
     fun aPropos(): String = "main/a_propos"
--- src/main/kotlin/fr/paugesjanes/repositories/ProjectRepository.kt
+++ src/main/kotlin/fr/paugesjanes/repositories/ProjectRepository.kt
@@ -2,8 +2,15 @@ package fr.paugesjanes.repositories
 
 import fr.paugesjanes.entities.Project
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 import java.util.*
 
 interface ProjectRepository : JpaRepository<Project, UUID> {
     fun findDistinctByTitleContainsOrSummaryContainsAllIgnoreCase(title: String, description: String): List<Project>
+
+    /* Crimes against humanity */
+    @Query(nativeQuery = true, value = """
+        SELECT * FROM (SELECT DISTINCT p.* FROM "project" AS p JOIN "user" AS u WHERE u."portfolio_id" = p."id") ORDER BY RAND() LIMIT 1
+    """)
+    fun findRandomPortfolio(): Project?
 }
