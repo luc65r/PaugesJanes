4987f32841215c16d53102055deddd383b0a4415
brenotgabr <brenotgabr@cy-tech.fr>
2023-11-05 22:52:07 +0100

    controler de favorite

--- src/main/kotlin/fr/paugesjanes/controllers/MainController.kt
+++ src/main/kotlin/fr/paugesjanes/controllers/MainController.kt
@@ -1,9 +1,11 @@
 package fr.paugesjanes.controllers
 
+import fr.paugesjanes.entities.Project
 import fr.paugesjanes.repositories.UserRepository
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
+import org.springframework.ui.set
 import org.springframework.web.bind.annotation.GetMapping
 
 @Controller
@@ -21,7 +23,10 @@ class MainController(
     fun contact(): String = "main/contact"
 
     @GetMapping("/favorite")
-    fun favorite(): String = "main/favorite"
+    fun favorite(model: Model): String {
+        model["projects"] = listOf<Project>()
+        return "main/favorite"
+    }
 
     @GetMapping("/profil")
     fun profil(): String = "main/profil"
